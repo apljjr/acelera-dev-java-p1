a9c11b7b8d290956fe72cfbac42f03021353e4ab
cleberson <clebrsonn@hotmail.com>
2019-02-02 14:01:29 -0300

    exceções

---
+++ src/main/java/br/com/codenation/aceleradev/rec/loja/exception/UsuarioInvalidoException.java
@@ -0,0 +1,23 @@
+package br.com.codenation.aceleradev.rec.loja.exception;
+
+public class UsuarioInvalidoException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public UsuarioInvalidoException(String message) {
+        super(message);
+    }
+
+    public UsuarioInvalidoException() {
+        super();
+    }
+
+    public UsuarioInvalidoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UsuarioInvalidoException(Throwable cause) {
+        super(cause);
+    }
+
+}
