90ee0b3121f62adff644427dfdadf0b3c0337f6d
Adeilson Junior <aplj.adeilson@gmail.com>
2019-02-02 14:08:36 -0300

    Main para chamadas ao sistema

--- src/main/java/br/com/codenation/aceleradev/rec/loja/Start.java
+++ src/main/java/br/com/codenation/aceleradev/rec/loja/Start.java
@@ -1,15 +1,21 @@
 package br.com.codenation.aceleradev.rec.loja;
-
-import br.com.codenation.aceleradev.rec.loja.connection.ConnectionFactory;
-
-import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Start {
     public static void main(String[] args) throws SQLException {
-        Connection conexao = new ConnectionFactory().getConnection();
-        System.out.println("Conectado!");
-        conexao.close();
-    }
+        int i = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite CPF para logar no sistema:");
+        while(sc.hasNext()){
+            i++;
+            
+            //// TODO: 02/02/2019
 
+            System.out.println("Comando: "+sc.next());
+            System.out.println("Próximo comando:");
+        }
+        sc.close();
+
+    }
 }
